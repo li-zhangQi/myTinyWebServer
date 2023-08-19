153189006cf057c0db0dc371642616a9c20d6854
Shinsam <2945639729@qq.com>
2023-08-19 21:01:56 +0800

    config

---
+++ config.cpp
@@ -0,0 +1,88 @@
+#include "config.h"
+
+Config::Config()
+{
+    //端口号,默认9006
+    PORT = 9006;
+
+    //日志写入方式，默认同步
+    LOGWrite = 0;
+
+    //触发组合模式,默认listenfd LT + connfd LT
+    TRIGMode = 0;
+
+    //listenfd触发模式，默认LT
+    LISTENTrigmode = 0;
+
+    //connfd触发模式，默认LT
+    CONNTrigmode = 0;
+
+    //优雅关闭链接，默认不使用
+    OPT_LINGER = 0;
+
+    //数据库连接池数量,默认8
+    sql_num = 8;
+
+    //线程池内的线程数量,默认8
+    thread_num = 8;
+
+    //关闭日志,默认不关闭
+    close_log = 0;
+
+    //并发模型,默认是proactor
+    actor_model = 0;
+}
+
+void Config::pares_arg(int argc, char*argv[])
+{
+    int opt;
+    const char *str = "p:l:m:o:s:t:c:a:";
+    while((opt = getopt(argc, argv, str)) != -1) // Linux下处理命令行参数的函数
+    {
+        switch(opt)
+        {
+            case 'p':
+            {
+                PORT = atoi(optarg); // 将字符串转换为对应的整数值。optarg为指向当前选项参数的指针
+                break;
+            }
+            case 'l':
+            {
+                LOGWrite = atoi(optarg);
+                break;
+            }
+            case 'm':
+            {
+                TRIGMode = atoi(optarg);
+                break;
+            }
+            case 'o':
+            {
+                OPT_LINGER = atoi(optarg);
+                break;
+            }
+            case 's':
+            {
+                sql_num = atoi(optarg);
+                break;
+            }
+            case 't':
+            {
+                thread_num = atoi(optarg);
+                break;
+            }
+            case 'c':
+            {
+                close_log = atoi(optarg);
+                break;
+            }
+            case 'a':
+            {
+                actor_model = atoi(optarg);
+                break;
+            }
+            default:
+                break;
+        }
+    }
+}
